9fda8d04c3ea0f4d0605a51885254669c1d35140
lequydinh <33369509+lequydinh@users.noreply.github.com>
2018-11-25 13:51:40 +0700

    View current user profile

--- src/main/java/com/mgmtp/blog/controller/LoginController.java
+++ src/main/java/com/mgmtp/blog/controller/LoginController.java
@@ -131,7 +131,24 @@ public class LoginController {
 		setSessionCookie(request, response, username);
 		return "redirect:/home";
 	}
-	
+
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public String showProfile(Model model, HttpServletRequest request) {
+		Cookie loginCookie = sessionService.checkLoginCookie(request);
+
+		if (loginCookie == null)
+			return "redirect:/login";
+
+		List<Session> sessions;
+		sessions = sessionService.checkSessionId(loginCookie.getValue());
+		if (sessions.isEmpty())
+			return "redirect:/login";
+
+		User user = userService.findByUsername(sessions.get(0).getUsername()).get(0);
+		model.addAttribute("user", user);
+		return "profile";
+	}
+
 	private void setSessionCookie(HttpServletRequest request, 
 			HttpServletResponse response, String username) {
 
